9f06de3c44d0c40bf5a9d6765d16f75eb3bf1498
WilliamDeckmann <u773560@rts-365.dk>
2021-08-23 12:28:18 +0200

    Opgave 4 struktur

--- Opgaver/Niveau2/01/js/index.js
+++ Opgaver/Niveau2/01/js/index.js
@@ -3,19 +3,31 @@ document.addEventListener("DOMContentLoaded", () => {
     // Element variables
     var button = document.querySelectorAll(".button");
 
-    // Buttons event listener
-    document.querySelector("#buttons").addEventListener("click", (event) => {
+    // Remove active class
+    function removeButtonClass() {
 
         // Remove class from inactive buttons
         for(let i = 0; i < button.length; i++) {
-
+    
             button[i].classList.remove("Button__background-color_light-green");
         };
+    };
+
+    // Target button
+    function targetButton() {
 
         // Check for target button
         if(event.target.className == "button") {
-
+    
             event.target.classList.toggle("Button__background-color_light-green");
         };
+    };
+
+    // Buttons event listener
+    document.querySelector("#buttons").addEventListener("click", () => {
+
+        // Initiators
+        removeButtonClass();
+        targetButton();
     });
 });
