76eddfac545a69d9169ae05df7a984937c1a0be3
WilliamDeckmann <u773560@rts-365.dk>
2021-08-23 09:52:31 +0200

    Opgave 2 features

--- Opgaver/Niveau1/02/js/index.js
+++ Opgaver/Niveau1/02/js/index.js
@@ -10,9 +10,31 @@ document.addEventListener("DOMContentLoaded", () => {
         return(a / b);
     };
 
+    // Divide a with b & and prevent 0
+    function beregnForhold2(a, b) {
+
+        // Check if either a or b is equal to 0
+        if(a == 0) {
+
+            return(null);
+        }else{
+            
+            if(b == 0) {
+    
+                return(null);
+            }else{
+                
+                return(a / b);
+            };
+        };
+
+    };
+
     // Function variable
     const returnerForhold = beregnForhold(argumentA, argumentB);
+    const returnerForhold2 = beregnForhold2(argumentA, argumentB);
 
     // Display result
     console.log(returnerForhold);
+    console.log(returnerForhold2);
 });
