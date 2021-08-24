a7a559de732991449f27d01cf721a8638cb23005
WilliamDeckmann <u773560@rts-365.dk>
2021-08-24 11:30:06 +0200

    Opgave 3 ekstra 2 forbedring

--- Opgaver/Niveau1/03_ekstra_2/js/index.js
+++ Opgaver/Niveau1/03_ekstra_2/js/index.js
@@ -1,23 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    // Hide specific elements via class
-    function skjulElementViaCssSelector(cssSelector, cssSelector2) {
+    // Element variables
+    const class_1 = document.querySelectorAll(".class_1");
 
-        skjulElement(cssSelector, cssSelector2);
+
+
+    // Hide specific elements via class
+    function skjulElementViaCssSelector(cssSelector) {
+
+        skjulElement(cssSelector);
     };
 
     // Initiator
-    skjulElementViaCssSelector(Element_number_2, Element_number_4);
+    skjulElementViaCssSelector(class_1);
 
     // Check to see if element is null
-    function skjulElement(element, element2) {
+    function skjulElement(element) {
 
-        if(element != null) {
-            element.style.display = "none";
-        }
+        // For each element with a specific class
+        for(let i = 0; i < element.length; i++) {
+    
+            if(element != null) {
+                element[i].style.display = "none";
+            }
+        };
 
-        if(element2 != null) {
-            element2.style.display = "none";
-        }
     };
 });
