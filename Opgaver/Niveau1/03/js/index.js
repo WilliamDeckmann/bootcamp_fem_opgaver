8872058504e3f78875218b0e8a0682a85dc56740
WilliamDeckmann <u773560@rts-365.dk>
2021-08-26 12:20:20 +0200

    Opgave 3 comments

--- Opgaver/Niveau1/03/js/index.js
+++ Opgaver/Niveau1/03/js/index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Hide specific elements via class
     function skjulElementViaCssSelector(cssSelector) {
 
+        // Set display mode to none
         cssSelector.style.display = "none";
     };
 
