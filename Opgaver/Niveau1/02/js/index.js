b858d2915be6900c1f49d832ada885ad3fb72f64
WilliamDeckmann <u773560@rts-365.dk>
2021-08-26 11:28:49 +0200

    Opgave 2 comments

--- Opgaver/Niveau1/02/js/index.js
+++ Opgaver/Niveau1/02/js/index.js
@@ -7,23 +7,28 @@ document.addEventListener("DOMContentLoaded", () => {
     // Divide a with b
     function beregnForhold(a, b) {
 
+        // Return the result
         return(a / b);
     };
 
     // Divide a with b & and prevent 0
     function beregnForhold2(a, b) {
 
-        // Check if either a or b is equal to 0
+        // Check if either a is equal to 0
         if(a == 0) {
 
+            // Return nothing
             return(null);
         }else{
             
+            // Check if either b is equal to 0
             if(b == 0) {
     
+                // Return nothing
                 return(null);
             }else{
                 
+                //Return the result
                 return(a / b);
             };
         };
